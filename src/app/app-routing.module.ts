6c21eeef2b35083838f00236de7b8f91f7b8ab4d
JadaDLetts <jletts77@gmail.com>
2020-09-05 00:32:57 -0400

    updated index and app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from "./index/index.component";
+import {LoginComponent} from "./login/login.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'signUp',component: SignUpComponent},
+  {path: 'home', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
