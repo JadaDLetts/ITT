75158092f731c30d2d390f8bf1104372ba26916e
JadaDLetts <jletts77@gmail.com>
2020-09-05 21:15:18 -0400

    updated app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {IndexComponent} from "./index/index.component";
 import {LoginComponent} from "./login/login.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signUp',component: SignUpComponent},
-   {path: '', component: IndexComponent}
+  {path: '', component: IndexComponent},
+  {path: 'dashboard/:userId', component: DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,23 @@ import {FormsModule} from "@angular/forms";
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import {UserService} from "./services/user.service.client";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
